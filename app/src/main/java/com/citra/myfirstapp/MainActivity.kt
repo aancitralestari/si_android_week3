0be1b47d7dd6b2915ac9a72deebbb5a922357be9
aancitralestari <aancitralestari@gmail.com>
2023-09-02 15:09:43 +0700

    first commit

---
+++ app/src/main/java/com/citra/myfirstapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.citra.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+// git config --global user.name "aancitralestari"
+// git config --global user.email "aancitralestari@gmail.com"
